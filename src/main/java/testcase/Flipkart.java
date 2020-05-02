af5c5b92d55335acdbc4a67de9604a10f3590ed1
MADANRAJ THANGARAJ <63949495+madantamil@users.noreply.github.com>
2020-05-03 02:31:11 +0530

    new changes

---
+++ src/main/java/testcase/Flipkart.java
@@ -0,0 +1,23 @@
+package testcase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+public class Flipkart {
+	public RemoteWebDriver driver;
+
+	@Test
+	public void execute() {
+
+		System.out.println("Success");
+
+	}
+
+}
